0931066f64ab72e034c2434ab6c50d3411178f37
Ashley Zink <pamperedzink@gmail.com>
2020-08-04 20:53:03 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ashley Zink";
+    int age = 35;
+    String state = "Arizona";
+    System.out.println("Name: " + name + " " + "Age: " + age + " " + "State: " + state);
+  }
+}
